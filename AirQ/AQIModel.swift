390b15491455971378896c7d12ee1b647e941a2b
Andrey Terechshenkov <aterechshenkov@gmail.com>
2021-01-15 09:06:55 +0600

    Add JSON example

--- AirQ/AQIModel.swift
+++ AirQ/AQIModel.swift
@@ -240,6 +240,291 @@ struct AQIDailyMetrics: Codable {
     var min: Double?
 }
 
-/* Response JSON example
-{"status":"ok","data":{"aqi":298,"idx":12774,"attributions":[{"url":"https://kz.usembassy.gov/","name":"Almaty Air Quality Monitor - US Embassy","logo":"US-StateDepartment.png"},{"url":"https://www.kazhydromet.kz/en/","name":"Қазгидромет - Kazhydromet","logo":"Kazakhstan.hydromet.png"},{"url":"https://waqi.info/","name":"World Air Quality Index Project"}],"city":{"geo":[43.222,76.851],"name":"Almaty, US Embassy, Kazakhstan (Алматы, АҚШ елшілігі)","url":"https://aqicn.org/city/kazakhstan/almaty/us-embassy"},"dominentpol":"pm10","iaqi":{"co":{"v":47.9},"dew":{"v":-14},"h":{"v":85},"no2":{"v":49.7},"p":{"v":1033},"pm10":{"v":298},"pm25":{"v":274},"so2":{"v":2.3},"t":{"v":-12},"w":{"v":0.5}},"time":{"s":"2020-12-28 12:00:00","tz":"+06:00","v":1609156800,"iso":"2020-12-28T12:00:00+06:00"},"forecast":{"daily":{"o3":[{"avg":4,"day":"2020-12-26","max":9,"min":1},{"avg":4,"day":"2020-12-27","max":15,"min":1},{"avg":9,"day":"2020-12-28","max":20,"min":1},{"avg":8,"day":"2020-12-29","max":20,"min":2},{"avg":5,"day":"2020-12-30","max":14,"min":1},{"avg":6,"day":"2020-12-31","max":16,"min":1},{"avg":1,"day":"2021-01-01","max":6,"min":1}],"pm10":[{"avg":17,"day":"2020-12-26","max":31,"min":13},{"avg":29,"day":"2020-12-27","max":37,"min":21},{"avg":15,"day":"2020-12-28","max":26,"min":4},{"avg":16,"day":"2020-12-29","max":22,"min":4},{"avg":17,"day":"2020-12-30","max":21,"min":12},{"avg":8,"day":"2020-12-31","max":12,"min":4},{"avg":5,"day":"2021-01-01","max":9,"min":3},{"avg":5,"day":"2021-01-02","max":9,"min":3},{"avg":8,"day":"2021-01-03","max":17,"min":2}],"pm25":[{"avg":64,"day":"2020-12-26","max":98,"min":52},{"avg":92,"day":"2020-12-27","max":113,"min":72},{"avg":55,"day":"2020-12-28","max":86,"min":29},{"avg":59,"day":"2020-12-29","max":75,"min":28},{"avg":60,"day":"2020-12-30","max":71,"min":48},{"avg":38,"day":"2020-12-31","max":52,"min":28},{"avg":28,"day":"2021-01-01","max":40,"min":23},{"avg":29,"day":"2021-01-02","max":41,"min":23},{"avg":37,"day":"2021-01-03","max":64,"min":21}],"uvi":[{"avg":0,"day":"2020-12-26","max":1,"min":0},{"avg":0,"day":"2020-12-27","max":1,"min":0},{"avg":0,"day":"2020-12-28","max":1,"min":0},{"avg":0,"day":"2020-12-29","max":1,"min":0},{"avg":0,"day":"2020-12-30","max":1,"min":0},{"avg":0,"day":"2020-12-31","max":1,"min":0},{"avg":0,"day":"2021-01-01","max":1,"min":0}]}},"debug":{"sync":"2020-12-28T15:39:36+09:00"}}}
+// MARK: Response JSON example
+/*
+ {
+     "status": "ok",
+     "data": {
+         "aqi": 61,
+         "idx": 12774,
+         "attributions": [
+             {
+                 "url": "https://kz.usembassy.gov/",
+                 "name": "Almaty Air Quality Monitor - US Embassy",
+                 "logo": "US-StateDepartment.png"
+             },
+             {
+                 "url": "https://www.kazhydromet.kz/en/",
+                 "name": "Қазгидромет - Kazhydromet",
+                 "logo": "Kazakhstan.hydromet.png"
+             },
+             {
+                 "url": "https://waqi.info/",
+                 "name": "World Air Quality Index Project"
+             }
+         ],
+         "city": {
+             "geo": [
+                 43.222,
+                 76.851
+             ],
+             "name": "Almaty, US Embassy, Kazakhstan (Алматы, АҚШ елшілігі)",
+             "url": "https://aqicn.org/city/kazakhstan/almaty/us-embassy"
+         },
+         "dominentpol": "pm25",
+         "iaqi": {
+             "co": {
+                 "v": 6.1
+             },
+             "dew": {
+                 "v": -13
+             },
+             "h": {
+                 "v": 78
+             },
+             "no2": {
+                 "v": 20.7
+             },
+             "p": {
+                 "v": 1031
+             },
+             "pm10": {
+                 "v": 51
+             },
+             "pm25": {
+                 "v": 61
+             },
+             "so2": {
+                 "v": 1.9
+             },
+             "t": {
+                 "v": -10
+             },
+             "w": {
+                 "v": 1
+             },
+             "wg": {
+                 "v": 6.6
+             }
+         },
+         "time": {
+             "s": "2021-01-14 18:00:00",
+             "tz": "+06:00",
+             "v": 1610647200,
+             "iso": "2021-01-14T18:00:00+06:00"
+         },
+         "forecast": {
+             "daily": {
+                 "o3": [
+                     {
+                         "avg": 7,
+                         "day": "2021-01-12",
+                         "max": 21,
+                         "min": 1
+                     },
+                     {
+                         "avg": 7,
+                         "day": "2021-01-13",
+                         "max": 15,
+                         "min": 3
+                     },
+                     {
+                         "avg": 5,
+                         "day": "2021-01-14",
+                         "max": 11,
+                         "min": 2
+                     },
+                     {
+                         "avg": 12,
+                         "day": "2021-01-15",
+                         "max": 22,
+                         "min": 4
+                     },
+                     {
+                         "avg": 16,
+                         "day": "2021-01-16",
+                         "max": 28,
+                         "min": 4
+                     },
+                     {
+                         "avg": 6,
+                         "day": "2021-01-17",
+                         "max": 16,
+                         "min": 1
+                     },
+                     {
+                         "avg": 4,
+                         "day": "2021-01-18",
+                         "max": 12,
+                         "min": 4
+                     }
+                 ],
+                 "pm10": [
+                     {
+                         "avg": 8,
+                         "day": "2021-01-12",
+                         "max": 13,
+                         "min": 3
+                     },
+                     {
+                         "avg": 11,
+                         "day": "2021-01-13",
+                         "max": 18,
+                         "min": 3
+                     },
+                     {
+                         "avg": 11,
+                         "day": "2021-01-14",
+                         "max": 14,
+                         "min": 8
+                     },
+                     {
+                         "avg": 17,
+                         "day": "2021-01-15",
+                         "max": 22,
+                         "min": 11
+                     },
+                     {
+                         "avg": 10,
+                         "day": "2021-01-16",
+                         "max": 13,
+                         "min": 6
+                     },
+                     {
+                         "avg": 22,
+                         "day": "2021-01-17",
+                         "max": 31,
+                         "min": 12
+                     },
+                     {
+                         "avg": 20,
+                         "day": "2021-01-18",
+                         "max": 29,
+                         "min": 18
+                     },
+                     {
+                         "avg": 18,
+                         "day": "2021-01-19",
+                         "max": 21,
+                         "min": 13
+                     },
+                     {
+                         "avg": 15,
+                         "day": "2021-01-20",
+                         "max": 18,
+                         "min": 12
+                     }
+                 ],
+                 "pm25": [
+                     {
+                         "avg": 35,
+                         "day": "2021-01-12",
+                         "max": 53,
+                         "min": 21
+                     },
+                     {
+                         "avg": 37,
+                         "day": "2021-01-13",
+                         "max": 56,
+                         "min": 23
+                     },
+                     {
+                         "avg": 44,
+                         "day": "2021-01-14",
+                         "max": 53,
+                         "min": 29
+                     },
+                     {
+                         "avg": 63,
+                         "day": "2021-01-15",
+                         "max": 75,
+                         "min": 47
+                     },
+                     {
+                         "avg": 28,
+                         "day": "2021-01-16",
+                         "max": 37,
+                         "min": 24
+                     },
+                     {
+                         "avg": 61,
+                         "day": "2021-01-17",
+                         "max": 75,
+                         "min": 29
+                     },
+                     {
+                         "avg": 61,
+                         "day": "2021-01-18",
+                         "max": 73,
+                         "min": 53
+                     },
+                     {
+                         "avg": 60,
+                         "day": "2021-01-19",
+                         "max": 71,
+                         "min": 52
+                     },
+                     {
+                         "avg": 58,
+                         "day": "2021-01-20",
+                         "max": 67,
+                         "min": 47
+                     }
+                 ],
+                 "uvi": [
+                     {
+                         "avg": 0,
+                         "day": "2021-01-12",
+                         "max": 2,
+                         "min": 0
+                     },
+                     {
+                         "avg": 0,
+                         "day": "2021-01-13",
+                         "max": 1,
+                         "min": 0
+                     },
+                     {
+                         "avg": 0,
+                         "day": "2021-01-14",
+                         "max": 2,
+                         "min": 0
+                     },
+                     {
+                         "avg": 0,
+                         "day": "2021-01-15",
+                         "max": 2,
+                         "min": 0
+                     },
+                     {
+                         "avg": 0,
+                         "day": "2021-01-16",
+                         "max": 2,
+                         "min": 0
+                     },
+                     {
+                         "avg": 0,
+                         "day": "2021-01-17",
+                         "max": 2,
+                         "min": 0
+                     },
+                     {
+                         "avg": 0,
+                         "day": "2021-01-18",
+                         "max": 1,
+                         "min": 0
+                     },
+                     {
+                         "avg": 0,
+                         "day": "2021-01-19",
+                         "max": 0,
+                         "min": 0
+                     }
+                 ]
+             }
+         },
+         "debug": {
+             "sync": "2021-01-14T22:28:06+09:00"
+         }
+     }
 */
